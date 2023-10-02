43f7043fbbd539a5f034d37a65bf4760e8df833a
Marvin Twilley <marvinmtwilley@gmail.com>
2023-10-01 21:34:06 -0500

    Adding some work from Section 2

---
+++ accounts/src/main/java/com/msbanks/accounts/controller/AccountsController.java
@@ -0,0 +1,76 @@
+package com.msbanks.accounts.controller;
+
+
+import com.msbanks.accounts.constants.AccountsConstants;
+import com.msbanks.accounts.dto.CustomerDto;
+import com.msbanks.accounts.dto.ErrorResponseDto;
+import com.msbanks.accounts.dto.ResponseDto;
+import com.msbanks.accounts.services.IAccountsServices;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Pattern;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping(path="/api",produces = (MediaType.APPLICATION_JSON_VALUE))
+@AllArgsConstructor
+public class AccountsController {
+
+    private IAccountsServices iAccountService;
+
+    @PostMapping("/create")
+    public ResponseEntity<ResponseDto> createAccount(@RequestBody CustomerDto customerDto){
+        iAccountService.createAccount(customerDto);
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(new ResponseDto(AccountsConstants.STATUS_201,AccountsConstants.MESSAGE_201));
+    }
+    @GetMapping("/fetch")
+    public ResponseEntity<CustomerDto> fetchAccountDetails(@RequestParam String mobileNumber){
+        CustomerDto customerDto = iAccountService.fetchAccount(mobileNumber);
+        return ResponseEntity.status(HttpStatus.OK).body(customerDto);
+    }
+
+    @PutMapping("/update")
+    public ResponseEntity<ResponseDto> updateAccountDetails(@Valid @RequestBody CustomerDto customerDto) {
+        boolean isUpdated = iAccountsService.updateAccount(customerDto);
+        if(isUpdated) {
+            return ResponseEntity
+                    .status(HttpStatus.OK)
+                    .body(new ResponseDto(AccountsConstants.STATUS_200, AccountsConstants.MESSAGE_200));
+        }else{
+            return ResponseEntity
+                    .status(HttpStatus.EXPECTATION_FAILED)
+                    .body(new ResponseDto(AccountsConstants.STATUS_417, AccountsConstants.MESSAGE_417_UPDATE));
+        }
+    }
+
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "HTTP Status OK"
+            ),
+            @ApiResponse(
+                    responseCode = "417",
+                    description = "Expectation Failed"
+            ),
+            @ApiResponse(
+                    responseCode = "500",
+                    description = "HTTP Status Internal Server Error",
+                    content = @Content(
+                            schema = @Schema(implementation = ErrorResponseDto.class)
+                    )
+            )
+    }
+    )
+}
---
+++ accounts/src/main/java/com/msbanks/accounts/repository/AccountsRepository.java
@@ -0,0 +1,13 @@
+package com.msbanks.accounts.repository;
+
+import com.msbanks.accounts.entity.Accounts;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface AccountsRepository extends JpaRepository<Accounts,Long> {
+    Optional<Accounts> findByCustomerId(Long customerId);
+    void deleteByCustomerId(Long customerId);
+}
