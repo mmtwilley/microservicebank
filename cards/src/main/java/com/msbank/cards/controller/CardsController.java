3e382d1c86483db76cdf8a7012128f7868510d8d
Marvin Twilley <marvinmtwilley@gmail.com>
2023-11-21 11:35:43 -0600

    Add a folder for configuration mangagement

--- cards/src/main/java/com/msbank/cards/controller/CardsController.java
+++ cards/src/main/java/com/msbank/cards/controller/CardsController.java
@@ -11,7 +11,7 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import jakarta.validation.valid;
+import jakarta.validation.Valid;
 import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
