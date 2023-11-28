8f04a485da924b9f9f34bbb29bcbe98d4b1b87ed
Marvin Twilley <marvinmtwilley@gmail.com>
2023-11-27 23:40:25 -0600

    Added a folder for config server

---
+++ v1-springboot/cards/src/main/java/com/msbank/cards/dto/CardsContactInfoDTO.java
@@ -0,0 +1,9 @@
+package com.msbank.cards.dto;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+import java.util.List;
+import java.util.Map;
+@ConfigurationProperties(prefix = "cards")
+public record CardsContactInfoDTO(String message, Map<String, String> contactDetails, List<String> onCallSupport) {
+}
