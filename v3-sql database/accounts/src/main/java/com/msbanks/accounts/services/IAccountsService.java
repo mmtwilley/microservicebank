6035bdc24d5ab15b773cfb9d29482021484bb214
Marvin Twilley <marvinmtwilley@gmail.com>
2024-01-27 12:59:03 -0600

    added mysql to the microservices

---
+++ v3-sql database/accounts/src/main/java/com/msbanks/accounts/services/IAccountsService.java
@@ -0,0 +1,33 @@
+package com.msbanks.accounts.services;
+
+import com.msbanks.accounts.dto.CustomerDto;
+
+public interface IAccountsService {
+    /**
+     *
+     * @param customerDto
+     */
+    void createAccount(CustomerDto customerDto);
+
+    /**
+     *
+     * @param mobileNumber
+     * @return Accounts Details based on a given Number
+     */
+    CustomerDto fetchAccount(String mobileNumber);
+
+    /**
+     *
+     * @param customerDto
+     * @return boolean indicating if the update of Account details is successful or not
+     */
+    boolean updateAccount(CustomerDto customerDto);
+
+    /**
+     *
+     * @param mobileAccount
+     * @return boolean indicating if the delete of Account details is successful or not
+     */
+    boolean deleteAccount(String mobileAccount);
+
+}
